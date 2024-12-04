38fd4564822927371cc186f051ef75cdb948cf0d
unknown <f.roukabi@gmail.com>
2024-12-04 22:36:12 +0100

    hot version

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,5 +1,4 @@
-docker build -t $1:latest $3
-docker tag $1:latest gcr.io/$2/$1:1.0
-docker tag $1:latest gcr.io/$2/$1:1.0
+docker build -t $1:1.0 $3
+docker tag $1:1.0 gcr.io/$2/$1:1.0
 docker push gcr.io/$2/$IMAGE_NAME:1.0
 
