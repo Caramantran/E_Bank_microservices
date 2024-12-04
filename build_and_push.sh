e570ea0003b3a743fab20b9f25875393dd824b4c
unknown <f.roukabi@gmail.com>
2024-12-04 22:40:21 +0100

    minor fix

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,4 +1,2 @@
-docker build -t $1:1.0 $3
-docker tag $1:1.0 gcr.io/$2/$1:1.0
-docker push gcr.io/$2/$IMAGE_NAME:1.0
-
+docker build -t europe-west1-docker.pkg.dev/$2/ebank-docker-repo/$1:1.0 $3
+docker push europe-west1-docker.pkg.dev/$2/ebank-docker-repo/$1:1.0
