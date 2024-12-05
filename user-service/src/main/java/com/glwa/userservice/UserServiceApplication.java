edb7fabb6be65251d51cd6514b68b9625a82136a
unknown <f.roukabi@gmail.com>
2024-12-05 03:32:20 +0100

    minor fixes

--- user-service/src/main/java/com/glwa/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/glwa/userservice/UserServiceApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@CrossOrigin(originPatterns = "*")
 public class UserServiceApplication implements CommandLineRunner {
     private final UserRepository userRepository;
 
